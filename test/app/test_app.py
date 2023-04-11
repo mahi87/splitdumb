c715f8def3cbb8eea487d65967aa58ac512d2465
Mahima choudhary <mahima.choudhary@jda.com>
2023-04-11 12:56:34 +0530

    format using black

--- test/app/test_app.py
+++ test/app/test_app.py
@@ -1,9 +1,10 @@
 import unittest
 from app import app
 
+
 class TestApp(unittest.TestCase):
     def test_hello(self):
         with app.test_client() as client:
             res = client.get("/")
             self.assertEqual(res.status_code, 200)
-            self.assertEqual(res.text, 'hello world')
+            self.assertEqual(res.text, "hello world")
