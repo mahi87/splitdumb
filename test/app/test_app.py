e0a550ae77646c2f6d763c1e8b809a0b1c9b2a22
Mahima choudhary <mahima.choudhary@jda.com>
2023-04-11 12:43:27 +0530

    initial commit

---
+++ test/app/test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        with app.test_client() as client:
+            res = client.get("/")
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(res.text, 'hello world')
